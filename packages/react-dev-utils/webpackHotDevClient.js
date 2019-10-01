1e5c9e2b6c1dc0ed915e90e1c67ae399c67387dd
Grady Kuhnline <github@heygrady.net>
2019-10-01 12:09:01 -0700

    adding env variables for sockHost and sockPort options

--- packages/react-dev-utils/webpackHotDevClient.js
+++ packages/react-dev-utils/webpackHotDevClient.js
@@ -61,8 +61,8 @@ if (module.hot && typeof module.hot.dispose === 'function') {
 var connection = new SockJS(
   url.format({
     protocol: window.location.protocol,
-    hostname: window.location.hostname,
-    port: window.location.port,
+    hostname: process.env.WDS_SOCKET_HOST || window.location.hostname,
+    port: process.env.WDS_SOCKET_PORT || window.location.port,
     pathname: process.env.WDS_SOCKET_PATH || '/sockjs-node',
   })
 );
